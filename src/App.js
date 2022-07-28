0d085393d0d48209558c99189dd42f7dd33db1e8
Yuto Yamakita <yuvto.0420@gmail.com>
2022-07-27 20:24:40 -0700

    Implemented User adding function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import './App.css';
-import {useState, useEffect} from "react"
+import {useState, useEffect, Fragment} from "react"
 import db from "./firebase/firebase"
-import { collection, getDocs, getFirestore, query, QuerySnapshot } from "firebase/firestore";
+import { collection, getDocs, getFirestore, query, QuerySnapshot, doc, getDoc, onSnapshot, addDoc } from "firebase/firestore";
+import { async } from '@firebase/util';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const db = getFirestore();
 
   useEffect(() =>{
-    const db = getFirestore();
+    //Multiple data
     const usersCollectionRef = query(collection(db, 'users'));
-    getDocs(usersCollectionRef)
-      .then((QuerySnapshot) => {
-        setUsers(QuerySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      });
+    // getDocs(usersCollectionRef)
+    //   .then((QuerySnapshot) => {
+    //     setUsers(QuerySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    //   });
+
+    //Fetch data every time, the database updates
+    const unsub = onSnapshot(usersCollectionRef, (QuerySnapshot) =>{
+      setUsers(QuerySnapshot.docs.map((doc) => ({
+        ...doc.data(), id: doc.id
+      })));
+    });
+    return unsub;
+
+    //Get Single data
+    // const singleUserData = query(doc(db, "users", "9QW3rE0GbIgBD3PWoOSJ"));
+    // getDoc(singleUserData).then((userData) => {
+    //   //exist method: Check if there is a data stored
+    //   if(userData.exists()){
+    //     console.log(userData.data(), userData.exists());
+    //   }else{
+    //     console.log('No data found')
+    //   }
+    // })
+
   }, []);
+
+  const submitHandler = async(e) =>{
+    e.preventDefault();
+    console.log(e.target.elements)
+    const {name, email} = e.target.elements;
+    console.log(name.value, email.value);
+    const usersRef = query(collection(db, "users"));
+    const documentRef = await addDoc(usersRef, {
+      name: name.value,
+      email: email.value
+    });
+    console.log(documentRef);
+  }
+
   console.log(users);
   return (
-    <div className="App">
-      {users && users.map((user) => (
-        <div key={user.id}>{user.name}</div>
-      ))}
-    </div>
+    <Fragment>
+      <form onSubmit={submitHandler} style={{ margin: "50px" }}>
+        <div>
+          <label>Name</label>
+          <input name="name" type="text" placeholder="name" required></input>
+        </div>
+        <div>
+          <label>email</label>
+          <input name="email" type="email" placeholder="email" required></input>
+        </div>
+        <div>
+          <button>Register</button>
+        </div>
+      </form>
+      <h2>User list</h2>
+      <div className="App">
+        {users && users.map((user) => <div key={user.id}>{user.name}</div>)}
+      </div>
+    </Fragment>
   );
 }
 
